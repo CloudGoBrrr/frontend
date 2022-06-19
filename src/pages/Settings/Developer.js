74ff266a933e389e8bb3a5fdec9cd7767f3dc5b6
ItzYanick <me@yanick.gay>
2022-06-19 19:29:54 +0200

    added webdav url

--- src/pages/Settings/Developer.js
+++ src/pages/Settings/Developer.js
@@ -1,8 +1,11 @@
 import { Card, Form } from "react-bootstrap";
+import path from "path-browserify";
 
+import { useAuth } from "../../components/context/AuthContext";
 import { useFeatureFlags } from "../../components/context/FeatureFlagContext";
 
 const Developer = () => {
+  const auth = useAuth();
   const featureFlags = useFeatureFlags();
 
   const apiURL =
@@ -13,18 +16,29 @@ const Developer = () => {
         process.env.REACT_APP_API_URL
       : process.env.REACT_APP_API_URL;
 
+  const webDavURL = path.join(apiURL, "webdav", auth.userDetails.username);
+
   return (
     <>
+      <Card className="mb-3">
+        <Card.Header>WebDAV</Card.Header>
+        <Card.Body>
+          <Form.Group>
+            <Form.Label>URL</Form.Label>
+            <Form.Control type="text" value={webDavURL} disabled />
+          </Form.Group>
+        </Card.Body>
+      </Card>
       <Card>
         <Card.Header>API</Card.Header>
         <Card.Body>
           <Form.Group>
-            <Form.Label>API URL</Form.Label>
+            <Form.Label>URL</Form.Label>
             <Form.Control type="text" value={apiURL} disabled />
           </Form.Group>
           <br />
           <Form.Group>
-            <Form.Label>API Version</Form.Label>
+            <Form.Label>Version</Form.Label>
             <Form.Control type="text" value={featureFlags.version} disabled />
           </Form.Group>
         </Card.Body>
